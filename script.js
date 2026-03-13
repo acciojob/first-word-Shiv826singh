91c5263b2b4ca6c5218d1341e751fe3b6e65fe23
SHIV PRAKASH SINGH <sshivprakash826@gmail.com>
2026-03-13 20:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 function firstWord(s) {
-  // your code here
+  if (!s) return "";
+
+  // Remove leading spaces
+  s = s.trimStart();
+
+  // Find first space
+  let index = s.indexOf(" ");
+
+  // If no space found, return whole string
+  if (index === -1) {
+    return s;
+  }
+
+  // Return substring before first space
+  return s.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
